fb8d392b0066df83210c921a07a54ee4032d8f38
barade <barade.barade@web.de>
2021-11-28 20:32:40 +0100

    Fix update script

--- updatemap.bat
+++ updatemap.bat
@@ -1,2 +1,2 @@
-copy C:\Users\Tamino\Documents\Projekte\wowtsr\wowr1.8.w3x "C:\Users\Tamino\Documents\Warcraft III\Maps\Download"
+copy C:\Users\Tamino\Documents\Projekte\wowtsr\wowr1.8.1.w3x "C:\Users\Tamino\Documents\Warcraft III\Maps\Download"
 pause
