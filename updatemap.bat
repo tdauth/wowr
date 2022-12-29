7b44a2754dbc34c21ff9da120173d2e7ea1bcc3c
Barade <barade.barade@web.de>
2022-12-29 19:17:47 +0100

    Release 3.2

--- updatemap.bat
+++ updatemap.bat
@@ -1,2 +1,2 @@
-copy C:\Users\Tamino\Documents\Projekte\wowr\wowr3.2.w3x "C:\Users\Tamino\Documents\Warcraft III\Maps\Download"
+copy C:\Users\Tamino\Documents\Projekte\wowr\wowr3.3.w3x "C:\Users\Tamino\Documents\Warcraft III\Maps\Download"
 pause
