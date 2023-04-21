b7ee5bdbfdb93c62fae1b9b084b6eba598f83598
Barade <barade.barade@web.de>
2023-04-21 23:46:28 +0200

    3.5 script

--- updatemap.bat
+++ updatemap.bat
@@ -1,2 +1,2 @@
-copy C:\Users\Tamino\Documents\Projekte\wowr\wowr3.4.w3x "C:\Users\Tamino\Documents\Warcraft III\Maps\Download"
+copy C:\Users\Tamino\Documents\Projekte\wowr\wowr3.5.w3x "C:\Users\Tamino\Documents\Warcraft III\Maps\Download"
 pause
