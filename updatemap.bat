d09a3b35f16364dcecc0c62cc8f5de0f4f7d1bc7
Barade <barade.barade@web.de>
2023-08-20 13:30:40 +0200

    Add Nerubian AI support

--- updatemap.bat
+++ updatemap.bat
@@ -1,2 +1,2 @@
-copy C:\Users\Tamino\Documents\Projekte\wowr\releases\wowr3.10.w3x "C:\Users\Tamino\Documents\Warcraft III\Maps\Download"
+copy C:\Users\Tamino\Documents\Projekte\wowr\releases\wowr3.11.w3x "C:\Users\Tamino\Documents\Warcraft III\Maps\Download"
 pause
