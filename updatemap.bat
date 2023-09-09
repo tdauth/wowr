99ebfd8e3c78187b0a1268cc43e5653b01db151f
Barade <barade.barade@web.de>
2023-09-09 14:15:31 +0200

    Fix Vrykul AI

--- updatemap.bat
+++ updatemap.bat
@@ -1,2 +1,3 @@
-copy C:\Users\Tamino\Documents\Projekte\wowr\releases\wowr3.11.w3x "C:\Users\Tamino\Documents\Warcraft III\Maps\Download"
+REM copy C:\Users\Tamino\Documents\Projekte\wowr\releases\wowr3.12.w3x "C:\Users\Tamino\Documents\Warcraft III\Maps\Download"
+copy D:\wowr\releases\wowr3.12.w3x "C:\Users\Tamino\Documents\Warcraft III\Maps\Download"
 pause
