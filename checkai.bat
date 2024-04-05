49b57e2e6c8e677b1fc658c53f4a0fc648e01e80
Barade <barade.barade@web.de>
2024-04-05 04:00:36 +0200

    AI scripts

--- checkai.bat
+++ checkai.bat
@@ -1,6 +1,6 @@
 REM D:\wowr\pjass.exe -h
 
-for %%f in (D:\wowr\wowr.w3x\war3mapImported\*.ai) do D:\wowr\pjass.exe D:\wowr\wc3reforged\common.j D:\wowr\wowr.w3x\scripts\common.ai %%f
+for %%f in (D:\wowr\wowr.w3x\war3mapImported\*.ai) do D:\wowr\pjass.exe D:\wowr\wc3reforged\common.j D:\wowr\wowr.w3x\scripts\common.ai "%%f"
 
 REM D:\wowr\pjass.exe D:\wowr\wc3reforged\common.j D:\wowr\wowr.w3x\scripts\common.ai D:\wowr\wc3reforged\blizzard.j D:\wowr\wowr.w3x\war3mapImported\*.ai
 pause
