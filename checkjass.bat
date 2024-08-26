733554d93f0d016dfec82d0fadf75ecbee624a52
Barade <barade.barade@web.de>
2024-08-26 20:57:10 +0200

    Bard profession

--- checkjass.bat
+++ checkjass.bat
@@ -1,5 +1,5 @@
-D:\wowr\pjass.exe -v
-for %%f in (D:\wowr\wowr.w3x\war3mapImported\*.ai) do D:\wowr\pjass.exe D:\wowr\wowr.w3x\scripts\common.j D:\wowr\wowr.w3x\scripts\common.ai "%%f"
-for %%f in (D:\wowr\wowr.w3x\*.j) do D:\wowr\pjass.exe D:\wowr\wowr.w3x\scripts\common.j D:\wowr\wowr.w3x\scripts\common.ai D:\wowr\wc3reforged\Blizzard.j "%%f"
+C:\Users\Tamino\Documents\Projekte\wowr\pjass.exe -v
+for %%f in (C:\Users\Tamino\Documents\Projekte\wowr\wowr.w3x\war3mapImported\*.ai) do C:\Users\Tamino\Documents\Projekte\wowr\pjass.exe C:\Users\Tamino\Documents\Projekte\wowr\wowr.w3x\scripts\common.j C:\Users\Tamino\Documents\Projekte\wowr\wowr.w3x\scripts\common.ai "%%f"
+for %%f in (C:\Users\Tamino\Documents\Projekte\wowr\wowr.w3x\*.j) do C:\Users\Tamino\Documents\Projekte\wowr\pjass.exe C:\Users\Tamino\Documents\Projekte\wowr\wowr.w3x\scripts\common.j C:\Users\Tamino\Documents\Projekte\wowr\wowr.w3x\scripts\common.ai C:\Users\Tamino\Documents\Projekte\wowr\wc3reforged\Blizzard.j "%%f"
 
 pause
--- updatemap.bat
+++ updatemap.bat
@@ -1,4 +1,4 @@
-copy C:\Users\Tamino\Documents\Projekte\wowr\releases\wowr3.24.w3x "C:\Users\Tamino\Documents\Warcraft III\Maps\Download"
+copy C:\Users\Tamino\Documents\Projekte\wowr\releases\wowr3.25.w3x "C:\Users\Tamino\Documents\Warcraft III\Maps\Download"
 REM copy D:\wowr\releases\wowr3.24.w3x "C:\Users\Tamino\Documents\Warcraft III\Maps\Download"
 REM copy D:\wowr\releases\wowr_elvenclan3.24.w3x "C:\Users\Tamino\Documents\Warcraft III\Maps\Download"
 REM copy D:\wowr\releases\wowr3.24.w3n "C:\Users\Tamino\Documents\Warcraft III\Campaigns"
