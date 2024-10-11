baf7e4d3c41ffbd7737e13d112500f14d3a8d0f2
Barade <barade.barade@web.de>
2024-10-11 20:25:39 +0200

    script

--- updatemap.bat
+++ updatemap.bat
@@ -1,5 +1,5 @@
-copy C:\Users\Tamino\Documents\Projekte\wowr\releases\wowr3.26.w3x "C:\Users\Tamino\Documents\Warcraft III\Maps\Download"
-REM copy D:\wowr\releases\wowr3.26.w3x "C:\Users\Tamino\Documents\Warcraft III\Maps\Download"
-REM copy D:\wowr\releases\wowr_elvenclan3.26.w3x "C:\Users\Tamino\Documents\Warcraft III\Maps\Download"
-REM copy D:\wowr\releases\wowr3.26.w3n "C:\Users\Tamino\Documents\Warcraft III\Campaigns"
+copy C:\Users\Tamino\Documents\Projekte\wowr\releases\wowr3.27.w3x "C:\Users\Tamino\Documents\Warcraft III\Maps\Download"
+REM copy D:\wowr\releases\wowr3.27.w3x "C:\Users\Tamino\Documents\Warcraft III\Maps\Download"
+REM copy D:\wowr\releases\wowr_elvenclan3.27.w3x "C:\Users\Tamino\Documents\Warcraft III\Maps\Download"
+REM copy D:\wowr\releases\wowr3.27.w3n "C:\Users\Tamino\Documents\Warcraft III\Campaigns"
 pause
