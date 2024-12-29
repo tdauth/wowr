cfccd8b24c1994e4e8679c16a3c054638071247b
Barade <barade.barade@web.de>
2024-12-29 03:38:37 +0100

    update

--- updatemap.bat
+++ updatemap.bat
@@ -1,4 +1,4 @@
-set version="3.37"
+set version="3.38"
 copy C:\Users\Tamino\Documents\Projekte\wowr\releases\wowr%version%.w3x "C:\Users\Tamino\Documents\Warcraft III\Maps\Download"
 REM copy D:\wowr\releases\wowr%version%.w3x "C:\Users\Tamino\Documents\Warcraft III\Maps\Download"
 REM copy D:\wowr\releases\wowr_elvenclan%version%.w3x "C:\Users\Tamino\Documents\Warcraft III\Maps\Download"
