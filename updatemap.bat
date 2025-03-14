d944d006d3d1ac8d0afa94ec4cd14ad33b50eb35
Barade <barade.barade@web.de>
2025-03-14 22:29:22 +0100

    AI tracing, Evolution and expansions

--- updatemap.bat
+++ updatemap.bat
@@ -1,4 +1,4 @@
-set version="3.38"
+set version="3.39"
 copy C:\Users\Tamino\Documents\Projekte\wowr\releases\wowr%version%.w3x "C:\Users\Tamino\Documents\Warcraft III\Maps\Download"
 REM copy D:\wowr\releases\wowr%version%.w3x "C:\Users\Tamino\Documents\Warcraft III\Maps\Download"
 REM copy D:\wowr\releases\wowr_elvenclan%version%.w3x "C:\Users\Tamino\Documents\Warcraft III\Maps\Download"
