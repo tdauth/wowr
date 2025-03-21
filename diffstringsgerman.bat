cff6bd04c7c1bb3018a22c4d35471af85a60dd75
Barade <barade.barade@web.de>
2025-03-21 16:39:54 +0100

    Melee range for mounts and strings

---
+++ diffstringsgerman.bat
@@ -0,0 +1,4 @@
+set MELD="C:\Program Files (x86)\Meld\Meld.exe"
+%MELD% "./wowr.w3x/war3mapImported/WoWReforgedStrings.fdf" "./wowr.w3x/_Locales/deDE.w3mod/war3mapImported/WoWReforgedStrings.fdf"
+%MELD% "./wowr.w3x/war3map.wts" "./wowr.w3x/_Locales/deDE.w3mod/war3map.wts"
+pause
