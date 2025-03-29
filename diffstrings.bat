50b70dc0904075dc09f723c0b9dc47aab0c57fa7
Barade <barade.barade@web.de>
2025-03-29 15:29:04 +0100

    wc3trans

--- diffstrings.bat
+++ diffstrings.bat
@@ -1,4 +1,5 @@
 set MELD="C:\Program Files (x86)\Meld\Meld.exe"
 %MELD% "./wowr.w3x/war3mapImported/WoWReforgedStrings.fdf" "./wowr.w3x/_Locales/deDE.w3mod/war3mapImported/WoWReforgedStrings.fdf"
+java -jar wc3trans.jar "./wowr.w3x/war3map.wts" "./wowr.w3x/_Locales/deDE.w3mod/war3map.wts"
 %MELD% "./wowr.w3x/war3map.wts" "./wowr.w3x/_Locales/deDE.w3mod/war3map.wts"
 pause
