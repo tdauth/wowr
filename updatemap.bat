a611d81ca6b3fed121aa0ca638f90ba790dcecdf
Barade <barade.barade@web.de>
2025-03-30 20:28:55 +0200

    update

--- updatemap.bat
+++ updatemap.bat
@@ -1,6 +1,3 @@
 set version="4.0"
-copy C:\Users\Tamino\Documents\Projekte\wowr\releases\wowr%version%.w3x "C:\Users\Tamino\Documents\Warcraft III\Maps\Download"
-REM copy D:\wowr\releases\wowr%version%.w3x "C:\Users\Tamino\Documents\Warcraft III\Maps\Download"
-REM copy D:\wowr\releases\wowr_elvenclan%version%.w3x "C:\Users\Tamino\Documents\Warcraft III\Maps\Download"
-REM copy D:\wowr\releases\wowr%version%.w3n "C:\Users\Tamino\Documents\Warcraft III\Campaigns"
+copy C:\Users\Tamino\Documents\Projekte\wowr\releases\wowr%version%.w3x "C:\Users\Tamino\Documents\Warcraft III\Maps\wowr"
 pause
