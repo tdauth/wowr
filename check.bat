56dc0cf159c2a4ed37022428d21bdebf434aa342
Barade <barade.barade@web.de>
2025-04-18 01:06:15 +0200

    check

--- check.bat
+++ check.bat
@@ -9,9 +9,10 @@ for /F "delims=" %%f in ('dir /s/b %map%\*.ai') do (
 for /F "delims=" %%f in ('dir /s/b %map%\*.pld') do %dir%\pjass.exe %map%\scripts\common.j %map%\scripts\common.ai "%%f"
 for /F "delims=" %%f in ('dir /s/b %map%\*.j') do (
    (Echo "%%f" | FIND /I "common.j" 1>NUL) || (
-        %dir%\pjass.exe %map%\scripts\common.j %map%\scripts\common.ai %dir%\wc3\Blizzard.j "%%f"
+        %dir%\pjass.exe %map%\scripts\common.j %dir%\wc3\Blizzard.j "%%f"
    )
 )
 for /F "delims=" %%f in ('dir /s/b %map%\*.fdf') do java -cp %dir%\* com.etheller.warsmash.fdfparser.Main "%%f"
+for /F "delims=" %%f in ('dir /s/b %dir%\out\*.j') do %dir%\pjass.exe %map%\scripts\common.j %dir%\wc3\Blizzard.j "%%f"
 
 pause
