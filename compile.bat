f622fa13127e677ed3860c26cfd071b1350f1916
Barade <barade.barade@web.de>
2025-04-17 23:25:47 +0200

    compile script

---
+++ compile.bat
@@ -0,0 +1,5 @@
+set dir="C:\Users\Tamino\Documents\Projekte\wowr"
+set map=%dir%\wowr.w3x
+jasshelper.exe --scriptonly %map%/scripts/common.j %dir%/wc3/reforged/scripts/blizzard.j %map%/war3map.j %dir%/out/war3map.j
+
+pause
