62aa48f0985cb2b43a074bcb6b572152d2433966
Barade <barade.barade@web.de>
2025-04-19 16:05:27 +0200

    scripts

--- check.bat
+++ check.bat
@@ -1,4 +1,4 @@
-set dir="C:\Users\Tamino\Documents\Projekte\wowr"
+set dir="C:\Users\%username%\Documents\Projekte\wowr"
 set map=%dir%\wowr.w3x
 %dir%\pjass.exe -v
 for /F "delims=" %%f in ('dir /s/b %map%\*.ai') do (
@@ -14,5 +14,8 @@ for /F "delims=" %%f in ('dir /s/b %map%\*.j') do (
 )
 for /F "delims=" %%f in ('dir /s/b %map%\*.fdf') do java -cp %dir%\* com.etheller.warsmash.fdfparser.Main "%%f"
 for /F "delims=" %%f in ('dir /s/b %dir%\out\*.j') do %dir%\pjass.exe %map%\scripts\common.j %dir%\wc3\Blizzard.j "%%f"
+
+%dir%\pjass.exe %map%\scripts\common.j %dir%\wc3\Blizzard.j "C:\Users\%username%\AppData\Local\Temp\buildmap.j"
+%dir%\pjass.exe %map%\scripts\common.j %dir%\wc3\Blizzard.j "C:\Users\%username%\Downloads\scripts.j"
 
 pause
--- compile.bat
+++ compile.bat
@@ -1,4 +1,4 @@
-set dir="C:\Users\Tamino\Documents\Projekte\wowr"
+set dir="C:\Users\%username%\Documents\Projekte\wowr"
 set map=%dir%\wowr.w3x
 jasshelper.exe --scriptonly %map%/scripts/common.j %dir%/wc3/reforged/scripts/blizzard.j %map%/war3map.j %dir%/out/war3map.j
 
