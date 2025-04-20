f5a688ef0baa2bfe6acac469d9bb5d28632f7a6d
Barade <barade.barade@web.de>
2025-04-20 18:24:45 +0200

    scripts

--- scripts/updatemap.bat
+++ scripts/updatemap.bat
@@ -1,3 +1,3 @@
 set version="4.0"
-copy C:\Users\Tamino\Documents\Projekte\wowr\releases\wowr%version%.w3x "C:\Users\Tamino\Documents\Warcraft III\Maps\wowr"
+copy ..\releases\wowr%version%.w3x "C:\Users\Tamino\Documents\Warcraft III\Maps\wowr"
 pause
--- scripts/updatepreload.bat
+++ scripts/updatepreload.bat
@@ -1,2 +1,2 @@
-copy "C:\Users\Tamino\Documents\Warcraft III\CustomMapData\PreloadWorldOfWarcraftReforged.pld" C:\Users\Tamino\Documents\Projekte\wowr\wowr.w3x\war3mapImported
+copy "C:\Users\Tamino\Documents\Warcraft III\CustomMapData\PreloadWorldOfWarcraftReforged.pld" ..\wowr.w3x\war3mapImported
 pause
