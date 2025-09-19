f79e2e217d26fdfa2c170d482bbf3ddf34ee4dda
Tamino Dauth <taminojdauth@gmail.com>
2025-09-19 13:55:36 +0200

    scripts

--- scripts/maptojson.ts
+++ scripts/maptojson.ts
@@ -1,11 +1,14 @@
 var Translator = require('wc3maptranslator');
+const fs = require('fs');
 
 const inputDir = process.argv[2];
 const outputDir = process.argv[3];
 console.log("inputDir: " + inputDir);
 console.log("outputDir: " + outputDir);
 
-Object.keys(Translator).forEach(key => console.log(key));
+//Object.keys(Translator).forEach(key => console.log(key));
 
-const abilitiesResult = Translator.ObjectsTranslator.warToJson(inputDir + '/war3map.w3a');
-fs.writeFileSync(outputDir + '/abilities.json', abilitiesResult.buffer);
+fs.readFile(inputDir + '/war3map.w3a', function(err, data) {
+    var abilitiesResult = new Translator.Objects.warToJson(data, 'abilities'); // << specify 'abilities' as first argument. pass in `data` from reading the file, not the file path
+    fs.writeFileSync(outputDir + '/abilities.json', JSON.stringify(abilitiesResult.json, null, 4)); // << use abilitiesResult.json, not abilitiesResult.buffer
+});
