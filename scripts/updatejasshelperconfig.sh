c705f051ca50e8987cea51e037f5a7c58a24c407
Tamino Dauth <taminojdauth@gmail.com>
2026-03-28 12:08:39 +0100

    updatejasshelperconfig.sh

---
+++ scripts/updatejasshelperconfig.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+cp jasshelper.conf "/home/tdauth/snap/steam/common/.local/share/Steam/steamapps/compatdata/2243394608/pfx/drive_c/Program Files (x86)/Warcraft III/_retail_/x86_64/JassHelper/"
