777ae2629a72fed0db6e51734872349b1a345b32
Tamino Dauth <taminojdauth@gmail.com>
2026-04-01 07:41:10 +0200

    Add paths

--- scripts/updatejasshelperconfig.sh
+++ scripts/updatejasshelperconfig.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+cp jasshelper.conf "/home/tdauth/snap/steam/common/.local/share/Steam/steamapps/compatdata/2243394608/pfx/drive_c/Program Files (x86)/Warcraft III/_ptr_/x86_64/JassHelper/"
 cp jasshelper.conf "/home/tdauth/snap/steam/common/.local/share/Steam/steamapps/compatdata/2243394608/pfx/drive_c/Program Files (x86)/Warcraft III/_retail_/x86_64/JassHelper/"
+cp jasshelper.conf "/home/tdauth/snap/steam/common/.local/share/Steam/steamapps/compatdata/2424345529/pfx/drive_c/users/steamuser/Documents/Warcraft III/JassHelper/"
