937d4ea4d4505f547e625a2eec6547c100950af4
Tamino Dauth <taminojdauth@gmail.com>
2026-05-17 23:20:52 +0200

    systems

--- scripts/warsmash.sh
+++ scripts/warsmash.sh
@@ -2,6 +2,11 @@
 DIR=$(pwd)
 INI_FILE="$DIR/warsmash.ini"
 MAP_DIR="$HOME/Dokumente/Projekte/wowr/wowr.w3x" # Directories require absolute file paths: https://github.com/Retera/WarsmashModEngine/issues/97
+#MAP_DIR="$HOME/Dokumente/Projekte/wowr/systems/baradeslog.w3x"
+#MAP_DIR="$HOME/Dokumente/Projekte/wowr/systems/baradescrafting.w3x"
+#MAP_DIR="$HOME/Dokumente/Projekte/wowr/systems/baradesblackarrow.w3x"
+#MAP_DIR="$HOME/Dokumente/Projekte/wowr/systems/baradesray.w3x"
+#MAP_DIR="$HOME/Dokumente/Projekte/wowr/systems/pocketfactory.w3x"
 #MAP_DIR="$DIR/../releases/wowr4.6.w3x"
 WARSMASH_DIR="$HOME/Dokumente/Projekte/WarsmashModEngine"
 cd "$WARSMASH_DIR"
