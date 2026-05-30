543f9da26d88b40985907e7ecb1ad02fa11dc039
Tamino Dauth <taminojdauth@gmail.com>
2026-05-30 17:52:26 +0200

    script

--- scripts/updatejasshelperconfig.sh
+++ scripts/updatejasshelperconfig.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-cp jasshelper.conf "/home/tdauth/snap/steam/common/.local/share/Steam/steamapps/compatdata/2243394608/pfx/drive_c/Program Files (x86)/Warcraft III/_ptr_/x86_64/JassHelper/"
-cp jasshelper.conf "/home/tdauth/snap/steam/common/.local/share/Steam/steamapps/compatdata/2243394608/pfx/drive_c/Program Files (x86)/Warcraft III/_retail_/x86_64/JassHelper/"
-cp jasshelper.conf "/home/tdauth/snap/steam/common/.local/share/Steam/steamapps/compatdata/2424345529/pfx/drive_c/users/steamuser/Documents/Warcraft III/JassHelper/"
+WC3_DIR_0="$HOME/snap/steam/common/.local/share/Steam/steamapps/compatdata/2243394608"
+WC3_DIR_1="$HOME/snap/steam/common/.local/share/Steam/steamapps/compatdata/2424345529"
+cp jasshelper.conf "$WC3_DIR_0/pfx/drive_c/Program Files (x86)/Warcraft III/_ptr_/x86_64/JassHelper/"
+cp jasshelper.conf "$WC3_DIR_0/pfx/drive_c/Program Files (x86)/Warcraft III/_retail_/x86_64/JassHelper/"
+cp jasshelper.conf "$WC3_DIR_1/pfx/drive_c/users/steamuser/Documents/Warcraft III/JassHelper/"
